9ad9d0198de8e9d9fea3b3df0a8c83e8ae0192b1
ganesh <ganeshsadhashivam@outlook.com>
2023-02-21 20:34:06 +0530

    Dsawithkunalclass8

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        System.out.println("Hello world!");
-//        System.out.println("My name is ganesh");
+       System.out.println("Hello world!");
+       System.out.println("My name is ganesh");
         Scanner input = new Scanner(System.in);
-      //  System.out.println(input.nextInt()); //for taking input of int values
-      //  System.out.println(input.nextLine()); //for taking input string from users
+        System.out.println(input.nextInt()); //for taking input of int values
+        System.out.println(input.nextLine()); //for taking input string from users
     }
 }
