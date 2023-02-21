b30dafb3e9b5b83c95cae1f23e23360ac0dcdbb2
ganesh <ganeshsadhashivam@outlook.com>
2023-02-21 20:31:22 +0530

    Dsawithkunalclass8

---
+++ src/Inputs.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("pls Enter val;ues");
+        int rollNo = input.nextInt();
+        System.out.println(rollNo);
+
+        int r = 234_000_000;
+        System.out.println(r);
+        String name = input.next();
+        System.out.println(name);
+       float marks = input.nextFloat();
+        System.out.println(marks);
+    }
+}
---
+++ src/Primitives.java
@@ -0,0 +1,12 @@
+public class Primitives {
+
+    public static void main(String[] args){
+//        int rollNo = 5;
+//        char letter = 'r';
+//        float marks = 98.99f;
+//        double largeDecimal = 454545454.45456;
+//        long largeInteger = 2343535466567675677L;
+//        boolean check = false;
+
+    }
+}
